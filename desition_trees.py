70cdfe208ed758c3ad46c7f774f6e1d853051087
QirongYou <162591165+QirongYou@users.noreply.github.com>
2024-03-16 18:50:33 +0800

    Update desition_trees.py

--- desition_trees.py
+++ desition_trees.py
@@ -22,7 +22,7 @@ target = df.iloc[:, 0]
 train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=0.2, random_state=42)
 
 # Creating and training the decision tree classifier
-model = DecisionTreeClassifier()  # You can tune parameters like max_depth, min_samples_split, etc.
+model = DecisionTreeClassifier()
 model.fit(train_x, train_y)
 
 # Predicting the test set results
